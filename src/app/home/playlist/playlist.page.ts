406cf3e1d1903fa7cde69c9295a63f68c43d8253
joaoellio <pedroka_xd@live.com>
2021-04-20 17:55:47 -0300

    Add mini player

--- src/app/home/playlist/playlist.page.ts
+++ src/app/home/playlist/playlist.page.ts
@@ -177,16 +177,6 @@ maximize() {
     this.currSong.play();
     this.isPlaying = true;
   }
-
-  cancel() {
-    document.getElementById("miniPlayer").style.bottom = "-100px";
-    this.currImage = "";
-    this.currTitle = "";
-    this.currSubtitle = "";
-    this.progress = 0;
-    this.currSong.pause();
-    this.isPlaying = false;
-  }
   
   touchStart() {
     this.isTouched = true;

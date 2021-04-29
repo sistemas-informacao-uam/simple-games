ac70ed262a3d8babc6b197e80255f85f51cc3005
guilherme <guilherme.gl1997@gmail.com>
2021-04-29 14:24:03 -0300

    points service implementado na store page

--- src/app/home/store/store.page.ts
+++ src/app/home/store/store.page.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { PointsService } from 'src/app/services/points.service';
 
 @Component({
   selector: 'app-store',
   templateUrl: './store.page.html',
   styleUrls: ['./store.page.scss'],
 })
+
 export class StorePage implements OnInit {
 
-  constructor() { }
+  globalPoints: number;
+
+  constructor(
+    private pointsService: PointsService
+  ) {
+    this.globalPoints = this.pointsService.globalPoints;
+  }
 
   ngOnInit() {
   }

c5c11c752ccbb9516e387f7c67c238073132a72a
Felipe Rodrigues <felipsilva2@hotmail.com>
2021-06-12 23:59:22 -0300

    Adicionar compartilhamento de conquistas

--- src/app/home/store/store.page.ts
+++ src/app/home/store/store.page.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { PointsService } from 'src/app/services/points.service';
 import { Dialog } from '@capacitor/dialog';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-store',
   templateUrl: './store.page.html',
   styleUrls: ['./store.page.scss'],
+  providers: [SocialSharing]
 })
 
 export class StorePage implements OnInit {
@@ -13,7 +15,8 @@ export class StorePage implements OnInit {
   globalPoints: number;
 
   constructor(
-    private pointsService: PointsService
+    private pointsService: PointsService,
+    private socialSharing: SocialSharing
   ) {
     this.globalPoints = this.pointsService.globalPoints;
   }
@@ -28,5 +31,9 @@ export class StorePage implements OnInit {
     });
 
     console.log('Confirmed:', value);
+  };
+
+  async CompartilharConquistas() {
+    this.socialSharing.share('Olhem minhas conquistas! Pontuação total: ' + this.globalPoints)
   };
 }
